b24b7994f39fb296bd66e0e562ffeb54c366fdf7
Danny Noonan <dnoonan@cern.ch>
2021-07-29 16:55:51 -0500

    update to include 40 bit words written to SRAM

--- ASICBlocks/eLinkProcessor.py
+++ ASICBlocks/eLinkProcessor.py
@@ -33,14 +33,31 @@ def unpackChannelData(vHex):
 
     adcm1 = (x>>20) & (2**10 - 1)
     adc_tot = (x>>10) & (2**10 - 1)
-    toa = (x) & (2**10 - 1)    
-    
+    toa = (x) & (2**10 - 1)
+
     return tc, tp, adcm1, adc_tot, toa
 
+# TODO
+# Order of this needs to be verified (just taking from wikipedia Hamming(7,4) page right now
+hammingCodes = {'0000':'0000000',
+                '1000':'1110000',
+                '0100':'0111100',
+                '0010':'0101010',
+                '1010':'1011010',
+                '0110':'1100110',
+                '1110':'0010110',
+                '0001':'1101001',
+                '1001':'0011001',
+                '0101':'0100101',
+                '1101':'1010101',
+                '0011':'1000011',
+                '1011':'0110011',
+                '0111':'0001111',
+                '1111':'1111111'}
 
 def formatChannelData(row, k, lam, beta, CE, CI, CIm1):
-    if row.Ch==-1: return ''
-    
+    if row.Ch==-1: return '',''
+
     TC = row.TC==1
     TP = row.TP==1
 
@@ -51,54 +68,77 @@ def formatChannelData(row, k, lam, beta, CE, CI, CIm1):
         word += '{0:010b}'.format(row.ADC_TOT)
         word += '{0:010b}'.format(row.TOA)
 
-        return word
-    
+        if TP:
+            word40 = word + hammingCodes['1100']
+        else:
+            word40 = word + hammingCodes['1000']
+
+        return word, word40
+
     if TP: #No ZS or BX-1 ZS applied, but TOA suppresed for TCTP=01
         word = '0010'
         word += '{0:010b}'.format(row.ADCm1)
         word += '{0:010b}'.format(row.ADC_TOT)
 
-        return word
+        word40 = word + '00000000' + hammingCodes['0010']
 
-    threshold = ((int(lam*row.CMAvg)>>6) + 
-                 (int(k*row.ADCm1)>>5) + 
+        return word, word40
+
+    threshold = ((int(lam*row.CMAvg)>>6) +
+                 (int(k*row.ADCm1)>>5) +
                  CI[row.Ch] )
-    
+
     passZS = (row.ADC_TOT + CE) > threshold
-    
+
     if not passZS: #fails ZS
         word = ''
-        return word
-    
+
+        word40 = '{0:032b}'.format(0) + hammingCodes['1111']
+
+        return word, word40
+
     passZSm1 = row.ADCm1 > (8*CIm1[row.Ch] + (int(beta * row.CMAvg)>>7))
-    
+
     passZSTOA = row.TOA>0
 
     if (passZSm1) and (not passZSTOA): #TOA ZS
         word = '0000'
         word += '{0:010b}'.format(row.ADCm1)
         word += '{0:010b}'.format(row.ADC_TOT)
-        return word
-    
+
+        word40 = word + '00000000' + hammingCodes['0000']
+
+        return word, word40
+
     if (not passZSm1) and (not passZSTOA): #ADC(-1) and TOA ZS
         word = '0001'
         word += '{0:010b}'.format(row.ADC_TOT)
         word += '00'
-        return word
-        
+
+        word40 = word + '00000000' + '00000000' + hammingCodes['0001']
+
+        return word, word40
+
     if (not passZSm1) and (passZSTOA): #ADC(-1) ZS
         word = '0011'
         word += '{0:010b}'.format(row.ADC_TOT)
         word += '{0:010b}'.format(row.TOA)
-        return word
-    
+
+        word40 = word + '00000000' + hammingCodes['0011']
+
+        return word, word40
+
     if passZSm1 and passZSTOA: #ADC(-1), ADC, TOA pass ZS
         word = '01'
         word += '{0:010b}'.format(row.ADCm1)
         word += '{0:010b}'.format(row.ADC_TOT)
         word += '{0:010b}'.format(row.TOA)
-        return word
-        
+
+        word40 = word + hammingCodes['0100']
+
+        return word, word40
+
+
 def eLinkProcessor(vals):
 
     isDataStart = findHeaderWord(vals)
@@ -116,7 +156,7 @@ def eLinkProcessor(vals):
 
     for j in range(len(readStart)):
         readCycle[readStart[j]:readStop[j]] = readPattern[:readLen[j]]
-    
+
     # parse header information, setup columns for this data
     headerData = np.where(readCycle==70,
                           np.vectorize(unpackHeader)(vals),
@@ -143,8 +183,8 @@ def eLinkProcessor(vals):
 
     dfLink = dfLink.fillna(method='ffill').astype(int)
 
-    # Need to encorprate these registes in a way that can be passed between.  
-    # Likely will be csv files, thought this should wait for Cristian's implementation as well 
+    # Need to encorprate these registes in a way that can be passed between.
+    # Likely will be csv files, thought this should wait for Cristian's implementation as well
     k= 0.03125
     lam=0.015625
     beta=0.03125
@@ -152,6 +192,8 @@ def eLinkProcessor(vals):
     CI = np.random.randint(10,size=37)
     CIm1 = np.random.randint(6,size=37)
 
-    dfLink['ChData'] = dfLink.apply(formatChannelData,args=(k, lam, beta, CE, CI, CIm1),axis=1)
+    channelData = dfLink.apply(formatChannelData,args=(k, lam, beta, CE, CI, CIm1),axis=1)
+
+    dfLink[['ChData_Short','ChData']] = pd.DataFrame(channelData.tolist(),columns=['ChData_Short','ChData'])
 
     return dfLink
