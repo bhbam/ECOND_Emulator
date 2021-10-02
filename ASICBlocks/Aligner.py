ed6c447e4667ce1a5d21417ef038b80aa479ee1c
Danny Noonan <dnoonan@cern.ch>
2021-10-02 12:08:04 -0500

    Add first implemenation of aligner block

---
+++ ASICBlocks/Aligner.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+def Aligner(dfERx, LinkResetRocD, LinkResetOutput='00000000', AlignerLatency=1, LinkResetLength=256):
+    linkResets = np.argwhere(LinkResetRocD==1).flatten()
+
+    N = len(LinkResetRocD)
+    dfChannelData = dfERx.copy()
+    for idx in linkResets:
+        dfChannelData.loc[idx:idx+LinkResetLength] = LinkResetOutput
+
+    dfChannelData.columns = [f'Aligner_Out_Ch{i}' for i in range(12)]
+    return dfChannelData.shift(AlignerLatency).fillna('00000000')
