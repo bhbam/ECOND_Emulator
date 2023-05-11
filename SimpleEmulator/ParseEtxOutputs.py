5c634f3efdf346f1ba37ee22e73e9c2376cb4cc4
Danny Noonan <daniel.christopher.noonan@cern.ch>
2023-05-11 10:53:26 -0500

    add more parsing functions, for splitting dataframe of eTx into packets

--- SimpleEmulator/ParseEtxOutputs.py
+++ SimpleEmulator/ParseEtxOutputs.py
@@ -12,12 +12,58 @@ def toHex(x):
         return f'{int(x,2):08X}'
 toHexV=np.vectorize(toHex)
 
+def parseOutputPackets(dfOutput, i2c):
+    #get outputs, select the active eTx, and flatten them into a continuous stream of data
+    outputStream=dfOutput.values[:,i2c.ETx_active][:,::-1].flatten()
+    outputStreamInt=np.vectorize(lambda x: int(x,16))(outputStream)
+
+    # check for idle pattern
+    isIDLE = (outputStreamInt>>8)==int(i2c.IdlePattern,16)
+    #check for output header
+    isOutputHeader = (outputStreamInt>>23)==int(i2c.HeaderMarker,16)
+    #packets are considered started if an output header is present, and the previous BX is an idle
+    isPacketStart=np.concatenate([[False],isOutputHeader[1:] & isIDLE[:-1]])
+
+    #get starting index
+    outputStartIndices=np.where(isPacketStart)[0]
+
+    packets=[]
+    for outputStartIdx in outputStartIndices:
+        HdrMarker, PayloadLength, P, E, HT, EBO, M, T, Hamming = parseHeaderWord0(outputStreamInt[outputStartIdx])
+        BX, L1A, Orb, S, RR, CRC=parseHeaderWord1(outputStreamInt[outputStartIdx+1])
+
+        packetOut=outputStream[outputStartIdx:(outputStartIdx+PayloadLength+2)]
+
+        packets.append(packetOut)
+
+    return packets
+
 def parseIdle(Idle):
     BuffStat=Idle&0x7
     Err=(Idle>>3)&0x7
     RR=(Idle>>6)&0b11
     Pattern=(Idle>>8)
     return Pattern, RR, Err, BuffStat
+
+def parseHeaderWords(HeaderWords, returnDict=False):
+    if type(HeaderWords[0]) in [str,np.str_,np.string_]:
+        hdr_0=int(HeaderWords[0],16)
+    elif type(HeaderWords[0]) in [int,np.int]:
+        hdr_0=HeaderWords[0]
+    if type(HeaderWords[1]) in [str,np.str_,np.string_]:
+        hdr_1=int(HeaderWords[1],16)
+    elif type(HeaderWords[1]) in [int,np.int]:
+        hdr_1=HeaderWords[1]
+
+    if returnDict:
+        hdrFields=parseHeaderWord0(hdr_0,returnDict=returnDict)
+        hdrFields.update(parseHeaderWord1(hdr_1,returnDict=returnDict))
+    else:
+        hdrFields=parseHeaderWord0(hdr_0)
+        hdrFields+=parseHeaderWord1(hdr_1)
+
+    return hdrFields
+
 
 def parseHeaderWord0(HeaderWord0, returnDict=False):
     HdrMarker=(HeaderWord0>>23)&0x1ff
@@ -30,7 +76,7 @@ def parseHeaderWord0(HeaderWord0, returnDict=False):
     T=(HeaderWord0>>6)&0x1
     Hamming=(HeaderWord0>>0)&0x3f
     if returnDict:
-        return {"HdrMarker":HdrMarker, 
+        return {"HdrMarker":HdrMarker,
                 "PayloadLength":PayloadLength,
                 "P":P,
                 "E":E,
@@ -107,4 +153,3 @@ def parseDAQLink(eLinkData,hdr_marker=15):
         crcGood[i]=crc(codecs.decode((''.join(eLinkData[i])), 'hex'))==0
 
     return goodHeaderTrailer, hammingErrors, orbitNum, eventNum, bunchNum, CM0, CM1, Tc, Tp, ADCm1, ADCorTOT, TOA, crcGood
-
